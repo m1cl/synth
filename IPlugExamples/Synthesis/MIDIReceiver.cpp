c9262512fac4a8083df985342067e4a74ca891a4
Michael Ajala <michael_ajala@yahoo.de>
2015-09-05 21:46:51 +0200

    MidiReceiver wurde eingefügt

---
+++ IPlugExamples/Synthesis/MIDIReceiver.cpp
@@ -0,0 +1,9 @@
+//
+//  MIDIReceiver.cpp
+//  Synthesis
+//
+//  Created by micl-THiNKPAD on 05.09.15.
+//
+//
+
+#include "MIDIReceiver.h"
---
+++ IPlugExamples/Synthesis/MIDIReceiver.h
@@ -0,0 +1,14 @@
+//
+//  MIDIReceiver.h
+//  Synthesis
+//
+//  Created by micl-THiNKPAD on 05.09.15.
+//
+//
+
+#ifndef __Synthesis__MIDIReceiver__
+#define __Synthesis__MIDIReceiver__
+
+#include <stdio.h>
+
+#endif /* defined(__Synthesis__MIDIReceiver__) */
